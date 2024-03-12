48d3cbff021915efc6947f2ba81956a402e38f0f
Elyse350 <mutonielyse35@gmail.com>
2024-03-12 08:48:19 +0200

    changesss

--- src/test/jestblogController.test.ts
+++ src/test/jestblogController.test.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import BlogController from '../controllers/blogController';
 import BlogModel from '../db/blogModel';
+import jest from 'jest'; // Import jest module
 
 describe('BlogController', () => {
   let mockRequest: Partial<Request>;
